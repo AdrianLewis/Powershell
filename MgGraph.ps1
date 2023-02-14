3fd0731b7ba1585e4d8d099b0cf38c6eb0351599
AdrianLewis <49079254+AdrianLewis@users.noreply.github.com>
2023-02-14 09:04:57 +0000

    Add files via upload

---
+++ MgGraph.ps1
@@ -0,0 +1,17 @@
+﻿# Set the number of days (180 in this case)
+$DaysOld = 180
+
+# Get the current date
+$CurrentDate = Get-Date
+
+# Connect to the Microsoft Graph API
+Connect-MgGraph -Scopes "User.Read.All"
+
+# Get all blocked Office 365 users
+$BlockedUsers = Get-MgUser -Filter "accountEnabled eq false"
+
+# Filter users based on when their accounts were created
+$OldBlockedUsers = $BlockedUsers | Where-Object {($CurrentDate - $_.WhenCreated.DateTime).Days -gt $DaysOld}
+
+# Display the results
+$OldBlockedUsers | Select-Object DisplayName, UserPrincipalName, WhenCreated, AccountEnabled
